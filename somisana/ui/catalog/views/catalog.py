2cdd46a67015ad2832645eb8002eceb63e97ec00
dylan <d.pivo@saeon.nrf.ac.za>
2025-04-24 12:24:54 +0200

    show catalog thumbnails from api path or link

--- somisana/ui/catalog/views/catalog.py
+++ somisana/ui/catalog/views/catalog.py
@@ -11,6 +11,8 @@ bp = Blueprint('catalog', __name__, static_folder=Path(__file__).parent.parent /
 @cli.view()
 @api.user()
 def index():
-    products = cli.get('/product/all_products')
+    products = cli.get('/product/catalog_products')
 
-    return render_template('catalog.html', products=products)
+    thumbnail_base_url = f'{api.api_url}/local_resources'
+
+    return render_template('catalog.html', products=products, thumbnail_base_url=thumbnail_base_url)
