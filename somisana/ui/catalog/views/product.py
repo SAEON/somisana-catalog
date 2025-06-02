1d825fba67ccd74e50589726bd4de2c833113087
dylan <d.pivo@saeon.nrf.ac.za>
2025-06-02 13:34:42 +0200

    show how projects supersede and are superseded

--- somisana/ui/catalog/views/product.py
+++ somisana/ui/catalog/views/product.py
@@ -15,8 +15,18 @@ def index(id):
 
     local_resource_base_url = f'{api.api_url}/local_resources'
 
+    superseded_by_product = {}
+    if product['superseded_by_product_id']:
+        superseded_by_product = cli.get(f'/product/{product['superseded_by_product_id']}')
+
+    superseded_product = {}
+    if product['superseded_product_id']:
+        superseded_product = cli.get(f'/product/{product['superseded_product_id']}')
+
     return render_template(
         'product.html',
         product=product,
+        superseded_by_product=superseded_by_product,
+        superseded_product=superseded_product,
         local_resource_base_url=local_resource_base_url
     )
