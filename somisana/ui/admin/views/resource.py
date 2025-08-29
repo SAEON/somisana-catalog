8459ce1a1d2f1b55d1ff1f692da0edfcbc87cae1
dylan <d.pivo@saeon.nrf.ac.za>
2025-08-29 11:36:31 +0200

    resource edit capabilities

--- somisana/ui/admin/views/resource.py
+++ somisana/ui/admin/views/resource.py
@@ -4,7 +4,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 from odp.lib.client import ODPAPIError
 from odp.ui.base import api
-from odp.ui.base.templates import delete_btn, create_btn
+from odp.ui.base.templates import delete_btn, create_btn, edit_btn
 from somisana.const import SOMISANAScope, EntityType, ResourceType
 from somisana.ui.admin.forms import ResourceForm, image_and_gif_files_allowed
 
@@ -51,6 +51,7 @@ def detail(id):
         'resource_detail.html',
         resource=resource,
         buttons=[
+            edit_btn(object_id=id),
             delete_btn(
                 object_id=id,
                 prompt_args=(resource['reference'],),
@@ -98,6 +99,58 @@ def create(
                 return response
 
     return render_template('resource_edit.html', form=form, entity_type=entity_type, entity_id=entity_id)
+
+
+@bp.route('<id>/edit/', methods=('GET', 'POST'))
+@api.view(SOMISANAScope.RESOURCE_ADMIN)
+def edit(
+        id: int
+):
+    resource = api.get(f'/resource/{id}')
+
+    if request.method == 'POST':
+        form = ResourceForm(request.form)
+        form.file.data = request.files.get('file')
+
+        if resource['reference_type'] == 'path':
+            form.reference.data = resource['reference']
+
+        resource_type = request.args.get('resource_type')
+    else:
+        form = ResourceForm(data=resource)
+
+        if resource['reference_type'] == 'path':
+            form.reference.render_kw = {'disabled': 'disabled'}
+            form.file.data = {'filename': resource['reference']}
+
+        resource_type = resource['resource_type']
+
+    set_form_options(form, resource_type)
+
+    if request.method == 'POST' and form.validate():
+        try:
+            if form.file.data:
+                api.put_files(
+                    path=f'/resource/{id}/?resource_type={form.resource_type.data}&title={form.title.data}',
+                    files={'file': (form.file.data.filename, form.file.data.stream)}
+                )
+            else:
+                api.post(
+                    path=f'/resource/{id}/',
+                    data={
+                        'title': form.title.data,
+                        'reference': form.reference.data,
+                        'resource_type': form.resource_type.data,
+                    }
+                )
+            flash(f'Resource {id} has been updated.', category='success')
+            return redirect(url_for('.detail', id=id))
+
+        except ODPAPIError as e:
+            if response := api.handle_error(e):
+                return response
+
+    return render_template('resource_edit.html', resource=resource, form=form)
 
 
 def get_post_create_redirect(entity_type: str, entity_id: int):
